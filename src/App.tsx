13a0dafa34a8e1ef597b63834e94f18cc4ae61e1
Prabodashan <prabodashan14@gmail.com>
2024-09-08 18:52:44 +0530

    Display genres and Create a generic data fetching hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
     <Grid
       templateAreas={{
         base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`, //124px
+        lg: `"nav nav" "aside main"`, //1024px
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
       }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">Aside</GridItem>
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
+        </GridItem>
       </Show>
       <GridItem area="main">
         <GameGrid />
